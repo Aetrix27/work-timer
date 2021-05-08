ca45d32b175a6b88485b9ff2fb5c392c5b8405b0
Aetrix27 <davedogg323@gmail.com>
2021-05-08 14:30:49 -0700

    Clock display works

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var second = setInterval(runTimer, 1000);
 var num = 60
+var minutes = 25
+
 function runTimer(){
-    num--
-    console.log(num);
+    if (num>0){
+        num--
+        console.log(minutes+":"+num);
+    }else if(num == 0){
+        minutes--
+        num=60
+    }
     
 }
 
-runTimer()
+runTimer();
 //Express View Engine for Handlebars
 //const exphbs  = require('express-handlebars');
 //app.engine('handlebars', exphbs());
