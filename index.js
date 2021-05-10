9138afb626b2f3e6bce413a950485e381328e99a
Aetrix27 <davedogg323@gmail.com>
2021-05-10 16:49:12 -0700

    Timer can be paused via keyboard
    keyboard input

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 var timerInterval = setInterval(runTimer, 1000);
+var stopped = false
 var keypress = require('keypress');
 var num = 0
 var minutes = 1
 
 
 function runTimer(){
-    if (num>0){
-        num--
-        console.log(minutes+":"+num);
-    }else if (minutes == 0 && num == 0){
-        console.log("TIME IS UP, TAKE A BREAK")
-        clearInterval(timerInterval)
-        return
-    }else if(num == 0){
-        console.log(minutes+": 00");
-        minutes--
-        num=60
+    if(stopped == false){
+        if (num>0){
+            num--
+            console.log(minutes+":"+num);
+        }else if (minutes == 0 && num == 0){
+            console.log("TIME IS UP, TAKE A BREAK")
+            clearInterval(timerInterval)
+            return
+        }else if(num == 0){
+            console.log(minutes+": 00");
+            minutes--
+            num=60
+        }
     }
     
 }
@@ -26,12 +29,21 @@ keypress(process.stdin);
 process.stdin.on('keypress', function (ch, key) {
   console.log('got "keypress"', key);
   if (key && key.name == 'space') {
-    process.stdin.pause();
+    if(stopped == false){
+        stopped = true
+    }else if(stopped == true){
+        stopped = false
+    }
+   
   }
 });
  
 process.stdin.setRawMode(true);
 process.stdin.resume();
+
+if (stopped == true){
+    stopped = false
+}
 
 runTimer();
 //Express View Engine for Handlebars
