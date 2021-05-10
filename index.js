c6d904b98fddb44eeab1765129fbe89c35165c17
Aetrix27 <davedogg323@gmail.com>
2021-05-10 12:22:24 -0700

    Spacebar input is detected

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-var second = setInterval(runTimer, 1000);
-var num = 60
-var minutes = 25
+var timerInterval = setInterval(runTimer, 1000);
+var keypress = require('keypress');
+var num = 0
+var minutes = 1
+
 
 function runTimer(){
     if (num>0){
         num--
         console.log(minutes+":"+num);
+    }else if (minutes == 0 && num == 0){
+        console.log("TIME IS UP, TAKE A BREAK")
+        clearInterval(timerInterval)
+        return
     }else if(num == 0){
+        console.log(minutes+": 00");
         minutes--
         num=60
     }
     
 }
+
+keypress(process.stdin);
+ 
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  console.log('got "keypress"', key);
+  if (key && key.name == 'space') {
+    process.stdin.pause();
+  }
+});
+ 
+process.stdin.setRawMode(true);
+process.stdin.resume();
 
 runTimer();
 //Express View Engine for Handlebars
