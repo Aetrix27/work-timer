e8d3e769417bff14b3b988b6a6d2afd9f3f34acb
Aetrix27 <davedogg323@gmail.com>
2021-05-12 21:12:02 -0700

    fixed timer display

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
-var timerInterval = setInterval(runTimer, 1000);
-var stopped = false
 var keypress = require('keypress');
-var num = 0
-var minutes = 1
 
+module.exports = function(){
+    //
+    var timerInterval = setInterval(runTimer, 100);
+    var stopped = false
+    var num = 0
+    var minutes = 1
 
-function runTimer(){
-    if(stopped == false){
-        if (num>0){
-            num--
-            console.log(minutes+":"+num);
-        }else if (minutes == 0 && num == 0){
-            console.log("TIME IS UP, TAKE A BREAK")
-            clearInterval(timerInterval)
-            return
-        }else if(num == 0){
-            console.log(minutes+": 00");
-            minutes--
-            num=60
+    function runTimer(){
+        if(stopped == false){
+            if (num>10){
+                num--
+                console.log(minutes+":"+num);
+            }else if (minutes == 0 && num == 0){
+                console.log("TIME IS UP, TAKE A BREAK")
+                clearInterval(timerInterval)
+                return
+            }else if(num == 0){
+                console.log(minutes+": 00");
+                minutes--
+                num=60
+            }else if(num <= 10 && num!=0){
+                num--
+                console.log(minutes+":0"+num);
+           
+            }
         }
+        
     }
+
+    keypress(process.stdin);
+    
+    // listen for the "keypress" event
+    process.stdin.on('keypress', function (ch, key) {
+        console.log('got "keypress"', key);
+        if (key && key.name == 'space') {
+            if(stopped == false){
+                stopped = true
+            }else if(stopped == true){
+                stopped = false
+            }
+        
+        }
+    });
+    
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+
+    if (stopped == true){
+        stopped = false
+    }
+
     
 }
 
-keypress(process.stdin);
- 
-// listen for the "keypress" event
-process.stdin.on('keypress', function (ch, key) {
-  console.log('got "keypress"', key);
-  if (key && key.name == 'space') {
-    if(stopped == false){
-        stopped = true
-    }else if(stopped == true){
-        stopped = false
-    }
-   
-  }
-});
- 
-process.stdin.setRawMode(true);
-process.stdin.resume();
-
-if (stopped == true){
-    stopped = false
-}
-
-runTimer();
-//Express View Engine for Handlebars
-//const exphbs  = require('express-handlebars');
-//app.engine('handlebars', exphbs());
-//app.set('view engine', 'handlebars');
-
+module.exports()
